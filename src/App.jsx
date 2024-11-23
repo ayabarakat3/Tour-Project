3f1fb7252727807b16b48b8da2dd3345614262e4
ayabarakat3 <ayabarakat14@gmail.com>
2024-11-23 17:09:54 -0500

    Style components for better user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import Gallery from './Gallery';
+import './Gallery.css';
 
 function App() {
   return (
     <div>
       <h1>Tour Gallery</h1>
+      <h2>It's tour time!</h2>
       <Gallery />
     </div>
   );
